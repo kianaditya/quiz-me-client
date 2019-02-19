44a92ad4dbe3857b8c24ae4e8a36c3b2c8fa5ae0
Aditya Naik <naik.aditya@gmail.com>
2019-02-19 12:37:22 +0100

    Updates tests, going green
    Co-authored-by: Gregory Kallai <gerg.kallai@gmail.com>

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,8 +1,12 @@
 describe('My First Test', function () {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/')
+    cy.server()
+    cy.route('GET','https://quiz-me-api.herokuapp.com/api/quiz','fixture:quiz.json')
+    
   })
-  it('Does not do much!', function () {
-    cy.contains('Hello World')
+  it('Shows quiz questions and answers on front page', function () {
+    cy.visit('http://localhost:3000/')
+    cy.contains('What is the name of the very first video uploaded to YouTube?')
+    cy.contains('Me at the zoo')
   })
 })
